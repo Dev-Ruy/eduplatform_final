4d02cd4983c9ccc97f0da19e123a7eadaecd1ac9
Dev-Ruy <ruybezerra@icloud.com>
2026-03-26 09:42:16 -0300

    Add Home component with navigation links

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>EduPlatform 🚀</h1>
+
+      <Link href="/login">Login</Link>
+      <br />
+      <Link href="/admin">Admin</Link>
+    </div>
+  )
+}
